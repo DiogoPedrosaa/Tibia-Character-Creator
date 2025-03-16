38c058478266638e4b4f5315d5d83b724a9740d8
DiogoPedrosaa <diogopedrosa07@gmail.com>
2025-03-16 18:50:46 -0300

    feat(character): criação do model de Character
    Criação do model de Character

---
+++ backend/models/characterModel.js
@@ -0,0 +1,41 @@
+const mongoose = require("mongoose");
+
+const characterSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    level: {
+        type: Number,
+        required: true
+    },
+    server: {
+        type: mongoose.Schema.Types.ObjectId, ref: "Server",
+        required: true
+    },
+    class: {
+        type: mongoose.Schema.Types.ObjectId, ref: "Class",
+        required: true
+    },
+    magic: {
+        type: Number
+    },
+    distance: {
+        type: Number
+    },
+    axe: {
+        type: Number
+    },
+    sword: {
+        type: Number
+    },
+    club: {
+        type: Number
+    },
+    shielding: {
+        type: Number
+    },
+
+});
+
+module.exports = mongoose.model("Character", characterSchema);
