367a3af8442db2423e3ab946db98b5028e754ee9
Diogo de Melo Monte Pedrosa <diogo.pedrosa@financas.pref>
2025-03-17 09:39:53 -0300

    fix(config) corrigindo a importação correta do caminho da pasta .env no serverConfig Adiciona a correção da importação do caminho da pasta .env

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI, {
+        await mongoose.connect("mongodb://localhost:27017/tibia_db", {
             useNewUrlParser: true,
             useUnifiedTopology: true
         });
