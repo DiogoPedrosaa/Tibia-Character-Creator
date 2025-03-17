56d3e9155aa6914238ebc3381be3021c4a7d186e
Diogo de Melo Monte Pedrosa <diogo.pedrosa@financas.pref>
2025-03-17 09:01:09 -0300

    feat(routes): criação das routes de Class
    Adiciona as routes de Class com as seguintes rotas:
    - router.get('/', getClasses)
    - router.post('/', createClass)
    - router.delete('/:id', updateClass)
    - router.patch('/:id', deleteClass)

---
+++ backend/routes/classRoutes.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const {getClasses,createClass,updateClass,deleteClass} = require('../controllers/classController');
+const router = express.Router();
+
+router.get('/', getClasses);
+router.post('/', createClass);
+router.put('/:id', updateClass);
+router.delete('/:id', deleteClass);
