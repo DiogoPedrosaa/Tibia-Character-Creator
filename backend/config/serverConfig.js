ab9c5fe2f08b4a814b2cbe71c629f1bd72c80000
Diogo de Melo Monte Pedrosa <diogo.pedrosa@financas.pref>
2025-03-17 09:09:51 -0300

    feat(config) criação da configuração do servidor
    Adiciona a configuração do servidor com as rotas da aplicação.

---
+++ backend/config/serverConfig.js
@@ -0,0 +1,19 @@
+require("dotenv").config();
+const express = require('express');
+const cors = require('cors');
+const connectDB = require('./db');
+
+const app = express();
+connectDB();
+
+app.use(express.json());
+app.use(cors());
+app.use(express.static("public"));
+
+app.use("/characters", require("../routes/characterRoutes"));
+app.use("/servers", require("../routes/serverRoutes"));
+app.use("/classes", require("../routes/classRoutes"));
+
+app.listen(process.env.PORT, () =>{
+    console.log(`servidor rodando na porta ${process.env.PORT}`);
+})
