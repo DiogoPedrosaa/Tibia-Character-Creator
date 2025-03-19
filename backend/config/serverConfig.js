c79c35060d4c9c76377e45ccc7d13c6f30802669
Diogo de Melo Monte Pedrosa <diogo.pedrosa@financas.pref>
2025-03-19 09:54:29 -0300

    feat(models) adicionado um model de Usuário para a implementação do sistema de autenticação com JWT. Adiciona um model de usuários ao banco com os seguintes campos: Username,Email,Password e Middleware para criptografia da senha no banco, e método para comparação de senhas.

--- backend/config/serverConfig.js
+++ backend/config/serverConfig.js
@@ -2,6 +2,7 @@ require("dotenv").config({ path: "../.env" });
 const express = require('express');
 const cors = require('cors');
 const connectDB = require('./db');
+const path = require('path');
 
 const app = express();
 connectDB();
@@ -9,6 +10,7 @@ connectDB();
 app.use(express.json());
 app.use(cors());
 app.use(express.static("public"));
+
 
 app.use("/characters", require("../routes/characterRoutes"));
 app.use("/servers", require("../routes/serverRoutes"));
